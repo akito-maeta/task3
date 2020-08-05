bc341b32d2c87538e6638c274e627a9779a16358
Apple <apple@appurunoMacBook-puro.local>
2020-08-05 17:13:13 +0900

    [task3]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+
+before_action :authenticate_user!
+
+  def new
+	  @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @books = Book.all
+    if @book.save
+    redirect_to book_path(@book), notice: 'You have creatad book successfully.'
+    else
+    redirect_to books_path, alert: "Some errors occured"
+    end
+  end
+
+  def index
+    @book = Book.new
+  	@books = Book.all
+    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'You have creatad book successfully.'
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b6ec88d05f2e5ec238b659d80971cbe4ec510eebdad5d1bf511db9090daeae278481257c70b2640bc57a88ec4370ecd098e467ee24c250eb80b6eb33ee4880c7'
